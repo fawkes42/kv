a3699f8ba2750785483b97a5556170b7d1c85474
Pedro Tozzi <pedrogbtozzi@gmail.com>
2024-09-01 17:58:17 -0300

    chore: Add tests for retrieving and returning values in KV.Bucket module

--- test/bucket_test.exs
+++ test/bucket_test.exs
@@ -3,9 +3,19 @@ defmodule KV.BucketTest do
 
   test "store values by key" do
     {:ok, bucket} = KV.Bucket.start_link()
-    assert KV.Bucket.get(bucket, "answer") == nil
 
     KV.Bucket.put(bucket, "answer", 42)
     assert KV.Bucket.put(bucket, "answer", 42) == :ok
+  end
+
+  test "retrieve values by key" do
+    {:ok, bucket} = KV.Bucket.start_link()
+    KV.Bucket.put(bucket, "answer", 42)
+    assert KV.Bucket.get(bucket, "answer") == 42
+  end
+
+  test "return nil when key is not found" do
+    {:ok, bucket} = KV.Bucket.start_link()
+    assert KV.Bucket.get(bucket, "answer") == nil
   end
 end
