8cf8f9a7847feb9d78155131931d2a34bc9b9259
Pedro Tozzi <pedrogbtozzi@gmail.com>
2024-09-02 23:53:27 -0300

    chore: add a setup "config" to  KV.BucketTest and pass the bucket as an argument to test

--- test/bucket_test.exs
+++ test/bucket_test.exs
@@ -1,21 +1,22 @@
 defmodule KV.BucketTest do
   use ExUnit.Case, async: true
 
-  test "store values by key" do
+  setup do
     {:ok, bucket} = KV.Bucket.start_link()
+    %{bucket: bucket}
+  end
 
+  test "store values by key", %{bucket: bucket} do
     KV.Bucket.put(bucket, "answer", 42)
     assert KV.Bucket.put(bucket, "answer", 42) == :ok
   end
 
-  test "retrieve values by key" do
-    {:ok, bucket} = KV.Bucket.start_link()
+  test "retrieve values by key", %{bucket: bucket} do
     KV.Bucket.put(bucket, "answer", 42)
     assert KV.Bucket.get(bucket, "answer") == 42
   end
 
-  test "return nil when key is not found" do
-    {:ok, bucket} = KV.Bucket.start_link()
+  test "return nil when key is not found", %{bucket: bucket} do
     assert KV.Bucket.get(bucket, "answer") == nil
   end
 end
